03f96bac15fb2a74db43b321c7dcf5236e7b1dfd
YeRin Ma <yealin0529@gmail.com>
2018-04-09 00:52:13 -0700

    test

---
+++ aaa.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+function main()
+{
+	read num1 opt num2 # 입력받은 값을 각각의 변수에 대입(공백을 기준으로 나눔)
+
+	case $opt in # opt 변수의 값이 다음중 무엇과 같은지
+
+		"+") # opt = "+"면
+
+			result=`expr $num1 "+" $num2` ;;
+
+		"-") # opt = "-"면
+
+			result=`expr $num1 "-" $num2` ;;
+
+		"x") # opt = "x"면
+
+			result=`expr $num1 "*" $num2` ;;
+
+		"/") # opt = "/"면
+
+			result=`expr $num1 "/" $num2` ;;
+
+		"%") # opt = "%"면
+
+			result=`expr $num1 "%" $num2` ;;
+
+	esac
+
+	echo $result # 결과값 result를 출력
+
+}
+main # main함수 실행
+
+
+
